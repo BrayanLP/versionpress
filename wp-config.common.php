f4c1331bfb248fa2495ef3d11c62c5755a61892b
Brayan LP <brayansystemlp@gmail.com>
2020-02-19 08:34:42 -0500

    feat: actualizando wordpress

--- wp-config.common.php
+++ wp-config.common.php
@@ -1,3 +1,5 @@
 <?php
 // Configuration common to all VersionPress environments, included from `wp-config.php`.
 // Learn more at https://docs.versionpress.net/en/getting-started/configuration
+
+// define('WP_CONTENT_DIR', 'custom_dir');
